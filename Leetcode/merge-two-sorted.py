2b51d4849f00d5066e6017fbea31e59e14bf62a8
Subham <das.jishu25@gmail.com>
2020-10-14 16:34:36 +0530

    Added leetcode

--- Leetcode/merge-two-sorted.py
+++ Leetcode/merge-two-sorted.py
@@ -24,10 +24,10 @@ Both l1 and l2 are sorted in non-decreasing order.
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         head = res = ListNode()
