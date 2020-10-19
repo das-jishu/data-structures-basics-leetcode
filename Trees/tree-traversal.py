85eb8688b979b2f297a1ff7350d150583fa1e1aa
Subham <das.jishu25@gmail.com>
2020-10-19 09:31:13 +0530

    Added tree traversal

---
+++ Trees/tree-traversal.py
@@ -0,0 +1,18 @@
+
+def preorder(tree):
+    if tree != None:
+        print(tree.getRootValue())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+
+def inorder(tree):
+    if tree != None:
+        inorder(tree.getLeftChild())
+        print(tree.getRootValue())
+        inorder(tree.getRightChild())
+
+def postorder(tree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootValue())
