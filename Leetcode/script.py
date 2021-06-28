f07edf2df1c69ddf9be044249d94dcac61ed0919
Subham <das.jishu25@gmail.com>
2021-06-28 08:55:30 +0530

    Added medium problem.

--- Leetcode/script.py
+++ Leetcode/script.py
@@ -1,5 +1,5 @@
 import os
 difficulty = input("Enter difficulty: ")
 os.system("git add .")
-os.system("git commit -m \"Add " + difficulty + " problem.\"")
+os.system("git commit -m \"Added " + difficulty + " problem.\"")
 os.system("git push -u origin master")
