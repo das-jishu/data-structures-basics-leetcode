d9422c7d4caf5935d942dbaa0273739b87bf20b9
Subham <das.jishu25@gmail.com>
2021-07-14 08:03:30 +0530

    cleanup

--- Searching-and-sorting/hashing.py
+++ Searching-and-sorting/hashing.py
@@ -1,39 +1,44 @@
 
 class HashTable(object):
-    def __init__(self, size):
-        self.size = size
+    def __init__(self):
+        self.size = 8
         self.slots = [None] * self.size
-        self.data = [None] * self.size
 
-    def put(self, key, data):
+    def put(self, key):
         hashvalue = self.hashfunction(key, len(self.slots))
 
         if self.slots[hashvalue] == None:
             self.slots[hashvalue] = key
-            self.data[hashvalue] = data
         else:
             if self.slots[hashvalue] == key:
-                self.slots[hashvalue] = data
+                pass
             else:
                 nextslot = self.rehash(hashvalue, len(self.slots))
                 while self.slots[nextslot] != None and self.slots[nextslot] != key:
                     nextslot = self.rehash(nextslot, len(self.slots))
                 if self.slots[nextslot] == None:
                     self.slots[nextslot] = key
-                    self.data[nextslot] = data
+                    
                 else:
-                    self.data[nextslot] = data
+                    pass
 
 
     def hashfunction(self, key, size):
-        return key % size
+        consonants = 0
+        digits = 0
+        for x in key:
+            if x.isdigit():
+                digits += int(x)
+            else:
+                consonants += ord(x)
+
+        return (consonants - digits) % 8
 
     def rehash(self, oldhash, size):
         return (oldhash + 1) % size
 
     def get(self, key):
         startslot = self.hashfunction(key, len(self.slots))
-        data = None
         stop = False
         found = False
         position = startslot
@@ -41,7 +46,7 @@ class HashTable(object):
         while self.slots[position] != None and not found and not stop:
             if self.slots[position] == key:
                 found = True
-                data = self.data[position]
+                data = position
 
             else:
                 position = self.rehash(position, len(self.slots))
@@ -51,9 +56,16 @@ class HashTable(object):
         return data
 
 if __name__ == "__main__":
-    h = HashTable(5)
-    for i in range(5):
-        h.put(i, i * 2)
-
-    for i in range(5):
-        print(h.get(i))
+    h = HashTable()
+    h.put("3C2SE11")
+    h.put("8B41")
+    h.put("DE4Z23DA")
+    h.put("J4")
+    h.put("6GOJE45")
+    print("Position of 3C2SE11:",h.get("3C2SE11"))
+    print("Position of 8B41:",h.get("8B41"))
+    print("Position of DE4Z23DA:",h.get("DE4Z23DA"))
+    print("Position of J4:",h.get("J4"))
+    print("Position of 6GOJE45:",h.get("6GOJE45"))
+    
+    
